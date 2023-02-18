2aa68c289d581e6b653a972859c51e26eedc48da
young <jem@netflix.com>
2023-02-17 22:34:44 -0800

    WIP

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,16 @@
 export default {
+    // Set output folder to read from GH pages
     dest: '/dist',
+    themeConfig: {
+      initialColorMode: 'dark',
+    },
+    menu: [
+      {
+        name: "Getting Started",
+        menu: [
+          "Getting Started",
+          "Welcome to your terminal"
+        ],
+      },
+    ]
   }
